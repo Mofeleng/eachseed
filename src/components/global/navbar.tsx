25ca8371180e06a024517a8029b00831afa56bd6
Mofeleng <mofelengmokgope@gmail.com>
2025-01-15 23:44:06 +0200

    Replaced manual navbar buttons with new links component

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import React from 'react'
+import CustomLink from '../ui/link'
 
 const Navbar = () => {
   return (
     <nav className='container mx-auto h-[100px] flex flex-row items-center justify-between'>
             <Image src="/logo.svg" alt='logo' width={150} height={150}/>
             <div className="flex gap-2">
-                <Link href="/" className='px-3 py-3 border border-slate-500 rounded-md min-w-[150px] text-center'>Start a campaign</Link>
-                <Link href="/" className='px-3 py-3 bg-[#088C29] text-white rounded-md  min-w-[150px] text-center'>Sign in</Link>
+                <CustomLink href="/founder/register" type="secondary" customStyles='text-center'>Start a campaign</CustomLink>
+                <CustomLink href="/signin" customStyles='text-center'>Sign in</CustomLink>
             </div>
     </nav>
   )
