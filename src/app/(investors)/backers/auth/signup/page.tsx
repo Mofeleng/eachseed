36dc1100eda14c2faabf18d79f68a0e546808b4a
Mofeleng <mofelengmokgope@gmail.com>
2025-02-05 01:29:04 +0200

    Called the signup form in the page .tsx file

--- src/app/(investors)/backers/auth/signup/page.tsx
+++ src/app/(investors)/backers/auth/signup/page.tsx
@@ -1,11 +1,12 @@
 
+import SignUpForm from '@/components/ui/sign-up-form'
 import React from 'react'
 
 const InvestorSignUp = () => {
   return (
     <div className="container mx-auto sm:px-5">
       <section className="py-16">
-        
+        <SignUpForm />
       </section>
       
     </div>
